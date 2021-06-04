0768aba84da9abcf73352ee81a7614d21b11640b
Taneesha <taneesha.chandrasena@acentura.com>
2021-06-05 01:30:59 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const subjectAPI = require('./src/api/subject.api');
+
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json);
+
+const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+
+    useCreateIndex: true,
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, (error) => {
+    if(error){
+        console.log('Database Error: ' + error.message);
+    }
+});
+
+mongoose.connection.once('open', ()=> {
+    console.log('Database Synced');
+});
+
+app.route('/').get((req, res) => {
+    res.send('SLIIT AF 2018 FINAl API');
+});
+
+app.use('/subject',subjectAPI());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT ${PORT}`);
+});
---
+++ src/controllers/subject.controller.js
@@ -0,0 +1,18 @@
+const Subject = require('../models/subjet.model');
+
+const createSubject = async (req, res) => {
+    if(req.body){
+        const subject = new Subject(req.body);
+        subject.save().then(data => {
+            //res.status(200).send({data: data});
+            res.json(data)
+        }).catch(error => {
+            //res.status(500).send({error: error.message});
+            console.log(error);
+        });
+    }
+}
+
+module.exports = {
+    createSubject
+};
