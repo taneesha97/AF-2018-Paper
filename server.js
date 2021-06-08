eeb2d051561dfdb4a78ee2eda0c1c92d284c225a
Taneesha <taneesha.chandrasena@acentura.com>
2021-06-09 01:15:59 +0530

    After error fixed

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const subjectAPI = require('./src/api/subject.api');
+const courseAPI = require('./src/api/course.api');
 
 
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -35,8 +36,10 @@ app.route('/').get((req, res) => {
 });
 
 app.use('/subject',subjectAPI());
-
+app.use('/course',courseAPI());
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on PORT ${PORT}`);
 });
+
+
--- src/controllers/subject.controller.js
+++ src/controllers/subject.controller.js
@@ -1,14 +1,18 @@
 const Subject = require('../models/subjet.model');
 
 const createSubject = async (req, res) => {
+    console.log("before if1");
     if(req.body){
+        console.log("inside if2");
         const subject = new Subject(req.body);
-        subject.save().then(data => {
-            //res.status(200).send({data: data});
-            res.json(data)
-        }).catch(error => {
-            //res.status(500).send({error: error.message});
-            console.log(error);
+        subject.save()
+            .then(data => {
+            res.status(200).send({data: data});
+            // res.json(data)
+        })
+            .catch(error => {
+            res.status(500).send({error: error.message});
+            // console.log(error);
         });
     }
 }
